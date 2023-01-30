822e8e5fce6b2278f250ca24e09ee395f172e5b2
IraquianRodrigues <iraquianrodrigues@hotmail.com>
2023-01-30 14:24:39 -0300

    commit deploy

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,20 +1,27 @@
-import styles from './styles.module.scss'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-import {FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi'
-import { GetStaticProps } from 'next'
-import {getPrismicClient} from '../../services/prismic'
-import Prismic from '@prismicio/client'
-import {RichText} from 'prismic-dom'
-import {useState} from 'react'
+import { GetStaticProps } from 'next';
+import { useState } from 'react';
 
-type Post ={
+import Head from 'next/head';
+
+import styles from './styles.module.scss';
+import Link from 'next/link';
+
+import Image from 'next/image';
+
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText} from 'prismic-dom'; 
+
+// https://png-pixel.com/
+
+import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi';
+
+type Post = {
   slug: string;
   title: string;
   cover: string;
   description: string;
-  upadatedAt: string;
+  updatedAt: string
 }
 
 interface PostsProps{
@@ -23,35 +30,36 @@ interface PostsProps{
   totalPage: string;
 }
 
-export default function Posts({posts: postsBlog, page, totalPage}: PostsProps){
-  const [currentPage, setcurrentPage] = useState(Number(page));
+export default function Posts({ posts: postsBlog, page, totalPage }: PostsProps){
+
+  const [currentPage, setCurrentPage] = useState(Number(page)); 
   const [posts, setPosts] = useState(postsBlog || []);
 
-  //buscar novos posts
+  //Buscar novos posts
   async function reqPost(pageNumber: number){
-    const prismic = getPrismicClient()
+    const prismic = getPrismicClient();
 
     const response = await prismic.query([
-      Prismic.predicates.at('document.type', 'post')
+      Prismic.Predicates.at('document.type', 'post')
     ], {
-      orderings: '[document.last_publication_date desc]', // ordernar pelo mais recente
-      fetch: ['post.title', 'post.description', 'post.cover'], // somente os itens que eu quero buscar
+      orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
+      fetch: ['post.title', 'post.description', 'post.cover'],
       pageSize: 3,
-      page: String(pageNumber)
+      page: String(pageNumber)  
     })
 
     return response;
   }
- 
+
   async function navigatePage(pageNumber: number){
     const response = await reqPost(pageNumber);
 
     if(response.results.length === 0){
-      return;
+    return;      
     }
 
-    const getPosts = response.results.map(post => {
-      return{
+    const getPosts = response.results.map( post => {
+      return {
         slug: post.uid,
         title: RichText.asText(post.data.title),
         description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
@@ -64,77 +72,83 @@ export default function Posts({posts: postsBlog, page, totalPage}: PostsProps){
       }
     })
 
-    setcurrentPage(pageNumber)
+    setCurrentPage(pageNumber)
     setPosts(getPosts);
+
   }
 
-    return(
-        <>
-        <Head>
-         <title>Blog | Code Developer</title>
-        </Head>  
-        <main className={styles.container}>
-            <div className={styles.posts}>
-            {posts.map( post => (
-                <Link key={post.slug} legacyBehavior href={`/post/${post.slug}`}>
-                <a key={post.slug}>
-                  <Image src={post.cover} 
-                  alt={post.title}
-                  width={720}
-                  height={410}
-                  quality={100}
-                  blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOMjU/+DwAEYAIgTdn/HQAAAABJRU5ErkJggg=='
-                  placeholder='blur'
-                  />
-                  <strong>{post.title}</strong>
-                  <time>{post.upadatedAt}</time>
-                  <p>{post.description}</p>             
-                </a>
-              </Link>
-            ))}
+  return(
+    <>
+     <Head>
+       <title>Blog | Sujeito Programador</title>
+     </Head>
+     <main className={styles.container}>
+       <div className={styles.posts}>
+        {posts.map( post => (
+          <Link legacyBehavior key={post.slug} href={`/posts/${post.slug}`}>
+            <a key={post.slug}>
+              <Image 
+                src={post.cover} 
+                alt={post.title}
+                width={720}
+                height={410}
+                quality={100}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
+              />
+              <strong>{post.title}</strong>
+              <time>{post.updatedAt}</time>
+              <p>{post.description}</p>
+            </a>
+          </Link>
+        ))}
 
-              <div className={styles.buttonNavigate}>
-                {Number(currentPage) >= 2 && (
-                  <div>
-                  <button onClick={() => navigatePage(1)}>
-                    <FiChevronLeft size={25} color="#fff"/>
-                  </button>
-                  <button onClick={() => navigatePage(Number(currentPage -1))}>
-                    <FiChevronsLeft size={25} color="#fff"/>
-                  </button>
-                </div>
-                )}
-               
-              {Number(currentPage) < Number(totalPage) && (
-                  <div>
-                  <button onClick={() => navigatePage(Number(currentPage + 1))}>
-                    <FiChevronRight size={25} color="#fff"/>
-                  </button>
-                  <button onClick={() => navigatePage(Number(totalPage))}>
-                    <FiChevronsRight size={25} color="#fff"/>
-                  </button>
-                </div>
-              )}
-    
+         <div className={styles.buttonNavigate}>
+            { Number(currentPage) >= 2 && (
+              <div>
+                <button onClick={ () => navigatePage(1) }>
+                  <FiChevronsLeft size={25} color="#FFF" />
+                </button>
+                <button onClick={ () => navigatePage(Number(currentPage - 1)) }>
+                  <FiChevronLeft size={25} color="#FFF" />
+                </button>
               </div>
-            </div>
-        </main>
-        </>
-    )
+            )}
+
+            { Number(currentPage) < Number(totalPage) && (
+              <div>
+                <button onClick={ () => navigatePage(Number(currentPage + 1)) }>
+                  <FiChevronRight size={25} color="#FFF" />
+                </button>
+                <button onClick={ () => navigatePage(Number(totalPage)) }>
+                  <FiChevronsRight size={25} color="#FFF" />
+                </button>
+              </div>
+            )}
+            
+         </div>
+       </div>
+     </main>
+    </>
+  )
 }
+
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
+
   const response = await prismic.query([
     Prismic.Predicates.at('document.type', 'post')
   ], {
-    orderings: '[document.last_publication_date desc]', // ordernar pelo mais recente
-    fetch: ['post.title', 'post.description', 'post.cover'], // somente os itens que eu quero buscar
-    pageSize: 3 // quantos posts vim por paginas
+    orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
+    fetch: ['post.title', 'post.description', 'post.cover'],
+    pageSize: 3
   })
 
-  const posts = response.results.map(post => {
-    return{
+  // console.log(JSON.stringify(response, null, 2))
+
+  const posts = response.results.map( post => {
+    return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
       description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
@@ -146,13 +160,13 @@ export const getStaticProps: GetStaticProps = async () => {
       })
     }
   })
-  
+
   return{
     props:{
       posts,
       page: response.page,
       totalPage: response.total_pages
     },
-    revalidate: 60 * 30 // atualiza a cada 30 min.
+    revalidate: 60 * 30 // Atualiza a cada 30 minutos.
   }
 }
